7e80f3bd3c28758b173e8b4a2be42405075e2e5f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-01 13:50:42 -0300

    Navegar de uma tela para outra

--- views/Inicio.js
+++ views/Inicio.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
-const Inicio = () => {
-  return <Text>Inicio</Text>;
+const Inicio = ({navigation}) => {
+  const visitarNosotros = () => {
+    navigation.navigate('Nosotros');
+  };
+
+  return (
+    <View style={styles.contenedor}>
+      <Text>Inicio</Text>
+      <Button title="Nosotros" onPress={() => visitarNosotros()} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Inicio;
