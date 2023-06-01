351744cc8937c84cef8b2b6e89b5981e6ea60cca
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-01 14:07:40 -0300

    Navegar para a página anterior

--- views/Nosotros.js
+++ views/Nosotros.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
-const Nosotros = () => {
-  return <Text>Nosotros</Text>;
+const Nosotros = ({navigation}) => {
+  const visitarInicio = () => {
+    navigation.navigate('Inicio');
+    // navigation.goBack();
+    // navigation.push('Inicio');
+  };
+
+  return (
+    <View style={styles.contenedor}>
+      <Text>Inicio</Text>
+      <Button title="Ir a Inicio" onPress={() => visitarInicio()} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Nosotros;
