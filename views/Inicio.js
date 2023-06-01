4b3fffac9a916fbd94493fef7b44228ce83fdbb6
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-01 13:39:38 -0300

    Initial commit

---
+++ views/Inicio.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Inicio = () => {
+  return <Text>Inicio</Text>;
+};
+
+export default Inicio;
---
+++ views/Nosotros.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Nosotros = () => {
+  return <Text>Nosotros</Text>;
+};
+
+export default Nosotros;
